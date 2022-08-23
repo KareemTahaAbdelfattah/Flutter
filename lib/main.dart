3e7febb1cd731c0182d70aeada4718345ae447d9
Kareem Taha <kimotaha52@gmail.com>
2022-08-23 22:19:32 +0200

    Todo Application with BloC

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:untitled/modules/Todo main screen/todo_main_screen.dart';
 import 'package:untitled/modules/bmi_screen/Calculator.dart';
 import 'package:untitled/modules/bmi_result/calculator_bmi_result.dart';
 import 'package:untitled/modules/counter/counter_screen.dart';
-import 'package:untitled/modules/counter_with_bloc/counter_with_bloc.dart';
 import 'package:untitled/modules/login/login_screen.dart';
 import 'package:untitled/modules/messenger_screen_with_list/messenger%20screen%20with%20list.dart';
 import 'package:untitled/modules/user_screen/use%20screen.dart';
@@ -22,7 +21,7 @@ class myapp extends StatelessWidget
   {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : CounterScreenWithBloc(),
+      home : TodoScreen(),
     );
   }
 
